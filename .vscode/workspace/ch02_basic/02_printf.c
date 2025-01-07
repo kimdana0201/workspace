4de1f09e8f595c396ae3bcc7740b9aed1f1bc50a
kimdana0201 <kdana0201@gmail.com>
2025-01-07 09:13:25 +0900

    Add files via upload

---
+++ .vscode/workspace/ch02_basic/02_printf.c
@@ -0,0 +1,18 @@
+#include <stdio.h>      //stdio.h를 가져와서 이 코드에 추가시켜라.
+
+int main() {            // () : 함수
+                        // ex : main() → main함수.
+                        // cf :  반드시 main()함수를 통해서 실행
+                        // {} : 블록문 → 해당 코드에 포함되는 내용
+                        // ex : {}는 main()함수 실행시 실행되는 내용들
+                        // C언어는 코드 끝에 ;(세미콜론)을 붙여줌. 코드 마지막이란 의미.
+
+                        // printf()함수 : (입력)값을 출력해주는 함수
+                        // print + format = printf
+                        // formatting : 형식을 바꿔라
+                        // ex : 182cm = 1.82m
+                        
+printf("Hi \t C");      // \t : 8칸(tab)
+printf("Hi \n C");      // \n : 한 줄 개행
+                        
+}
